020af15d0bc48f8e536063af3cb8e933ce420156
arshad012 <forarshad99@gmail.com>
2025-09-22 22:17:17 +0530

    updated locahost URL with global URL

--- frontend/src/constants/api/index.js
+++ frontend/src/constants/api/index.js
@@ -1,5 +1,6 @@
 
-export const BASE_URL = 'http://localhost:3000/api/';
+// export const BASE_URL = 'http://localhost:3000/api/';
+export const BASE_URL = 'https://quizzy-app-o0mr.onrender.com/';
 
 export * from './templates'
 export * from './assesments'
